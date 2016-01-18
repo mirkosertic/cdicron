aa7f7ea5404766e77fb5242b51da1f4685d1a293
Mirko Sertic <mirko.sertic@web.de>
2016-01-18 21:07:40 +0100

    Initial-work-in-progress-commit

--- quartzscheduler/src/main/java/de/mirkosertic/timedcdi/quartzscheduler/QuartzScheduler.java
+++ quartzscheduler/src/main/java/de/mirkosertic/timedcdi/quartzscheduler/QuartzScheduler.java
@@ -2,10 +2,7 @@ package de.mirkosertic.timedcdi.quartzscheduler;
 
 import de.mirkosertic.timedcdi.api.JobScheduler;
 
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SchedulerFactory;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import javax.annotation.PreDestroy;
@@ -13,8 +10,24 @@ import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.AfterDeploymentValidation;
 import javax.inject.Singleton;
 
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.CronScheduleBuilder.*;
+
 @Singleton
 public class QuartzScheduler implements JobScheduler {
+
+    public static class RunnableWrapperJob implements Job {
+
+        public static final String KEY = "runnable";
+
+        @Override
+        public void execute(JobExecutionContext aContext) throws JobExecutionException {
+            JobDataMap theDataMap = aContext.getJobDetail().getJobDataMap();
+            Runnable theRunnable = (Runnable) theDataMap.get(KEY);
+            theRunnable.run();
+        }
+    }
 
     private final Scheduler scheduler;
 
@@ -25,6 +38,15 @@ public class QuartzScheduler implements JobScheduler {
 
     @Override
     public void schedule(String aCronExpression, Runnable aRunnable) {
+        JobDataMap theJobDataMap = new JobDataMap();
+        theJobDataMap.put(RunnableWrapperJob.KEY, aRunnable);
+        JobDetail theDetails = newJob(RunnableWrapperJob.class).setJobData(theJobDataMap).build();
+        Trigger theTrigger = newTrigger().withSchedule(cronSchedule(aCronExpression)).build();
+        try {
+            scheduler.scheduleJob(theDetails, theTrigger);
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void afterInitialized(@Observes AfterDeploymentValidation aAfterDeploymentValidation) throws SchedulerException {
