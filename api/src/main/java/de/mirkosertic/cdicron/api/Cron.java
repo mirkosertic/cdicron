1ae673707c8df7d2a733f0a93e575992ad498e7a
sertic <sertic@thalia.de>
2016-01-20 13:48:33 +0100

    Corrected a typo

--- api/src/main/java/de/mirkosertic/cdicron/api/Cron.java
+++ api/src/main/java/de/mirkosertic/cdicron/api/Cron.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 /**
  * This marker annotation can be placed at any cdi managed beans instance method.
  *
- * The method will be called as described by the ceonExpression.
+ * The method will be called as described by the cronExpression.
  */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
